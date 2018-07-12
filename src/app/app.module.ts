2904854476ef943184525f811cc0fd7dd267f857
Essakiappan <SudalaiMuthuPillai.Essakiappan@ad.agi>
2018-07-12 11:11:18 +0530

    Removed fixed-top from navbar that caused positioning issue with elements.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ParamselectorComponent } from './home/paramselector/paramselector.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
