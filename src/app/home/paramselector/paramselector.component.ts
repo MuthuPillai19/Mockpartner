1008b50e4478068a69e6f614e765eddbb82f9975
Essakiappan <SudalaiMuthuPillai.Essakiappan@ad.agi>
2018-07-13 17:01:33 +0530

    Added the functionality for getting language code and Partner code.

--- src/app/home/paramselector/paramselector.component.ts
+++ src/app/home/paramselector/paramselector.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paramselector.component.css']
 })
 export class ParamselectorComponent implements OnInit {
+  languages = ["en_US", "en_GB", "fr_CA", "bg_BG", "it_IT"];
+  partnerCodes = ["mockpartner", "mockpartner2", "mockpartner3", "mockpartner4"];
 
   constructor() { }
 
